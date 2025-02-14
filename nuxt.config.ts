be511cad7fb0e7039eaca73bd93b19965b457959
Type-32 <87076491+Type-32@users.noreply.github.com>
2025-02-14 15:00:32 +0800

    init: init

---
+++ nuxt.config.ts
@@ -0,0 +1,39 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    devtools: { enabled: true },
+    // Enable SSG
+    ssr: false,
+    // Enables the development server to be discoverable by other devices when running on iOS physical devices
+    devServer: { host: process.env.TAURI_DEV_HOST || 'localhost' },
+    vite: {
+        // Better support for Tauri CLI output
+        clearScreen: false,
+        // Enable environment variables
+        // Additional environment variables can be found at
+        // https://v2.tauri.app/reference/environment-variables/
+        envPrefix: ['VITE_', 'TAURI_'],
+        server: {
+            // Tauri requires a consistent port
+            strictPort: true,
+        },
+    },
+
+    modules: [
+        '@nuxt/ui',
+        '@nuxt/eslint',
+    ],
+
+    css: ['~/assets/css/main.css'],
+
+    future: {
+        compatibilityVersion: 4,
+    },
+
+    compatibilityDate: '2024-11-27',
+    fonts: {
+        families: [
+            { name: 'Inter' },
+            { name: 'Noto Sans Simplified Chinese' }
+        ]
+    }
+})
