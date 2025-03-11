ef0f565b6a73d0971dc2f019e46e443aef7ffb79
Type-32 <87076491+Type-32@users.noreply.github.com>
2025-03-11 15:19:53 +0800

    chore: clearing stuff out, moving stuff to layout

--- app/layouts/PagesDirectory.vue
+++ app/layouts/PagesDirectory.vue
@@ -1,187 +1,96 @@
 <script setup lang="ts">
-// import {
-//     ScrollAreaRoot, ScrollAreaScrollbar, ScrollAreaThumb, ScrollAreaViewport,
-//     SplitterGroup,
-//     SplitterPanel,
-//     SplitterResizeHandle,
-//     TreeItem,
-//     TreeRoot,
-//     TreeVirtualizer,
-// } from 'reka-ui'
-// import type { SlatePage } from '~/slate.types'
-//
-// const $route = useRoute()
-// const $slate = useSlateFile()
-// const $slateCommon = useSlateCommon()
-//
-// const currentPage = computed(() => $route.params.pageId as string)
-// const pages = computed(() => $slate.getCurrentSlateDoc().value?.pages)
-//
-// // Track expanded state
-// const expandedItems = ref<string[]>([])
-// const $t = useToast()
-//
-// function remap() {
-//     const path = $slate.getCurrentNestedPageDirs(currentPage)
-//     expandedItems.value = path.map(node => node.uuid)
-// }
-// onMounted(() => {
-//     remap()
-//     $slate.subscribeOnPageReindex(remap)
-// })
-//
-// const handleRename = (uuid: string, currentName: string) => {
-//     $slateCommon.renamePage(uuid, currentName)
-// }
-//
-// const handleDelete = (uuid: string, recursive: boolean) => {
-//     $slateCommon.deletePage(uuid, recursive)
-// }
-//
-// const handleCreatePage = async (mode: 'root' | 'current' | 'under', targetPageUUID: string) => {
-//     await $slateCommon.createPage(mode, targetPageUUID)
-// }
-//
-// const handleChangeIcon = (uuid: string) => {
-//     $slateCommon.changeIcon(uuid)
-// }
+const $route = useRoute()
+const $slate = useSlateFile()
+const $slateCommon = useSlateCommon()
 
-// console.log("wh")
+const breadcrumbs = computed(() => {
+    let arr = $slate.getCurrentNestedPageDirsPerformant($pageId).map((i: any) => {
+        return {
+            slot: 'page',
+            label: i.name,
+            icon: i.icon,
+            to: `/document/${i.uuid}`,
+        }
+    })
+    if (arr.length > 1){
+        let concat = {
+            slot: 'dropdown',
+            icon: 'lucide:ellipsis',
+            label: '',
+            children: [
+                ...arr.slice(0, arr.length-1)
+            ]
+        }
+        return [
+            {
+                icon: '',
+                slot: 'document',
+                label: $slate.getFileName(),
+            },
+            concat,
+            arr[arr.length-1],
+        ]
+    }
+
+    return [
+        {
+            icon: '',
+            slot: 'document',
+            label: $slate.getFileName(),
+        },
+        ...arr,
+    ]
+})
+
+const $pageId = computed(() => $route.params.pageId as string || '')
+
 </script>
 
 <template>
-<!--    <SplitterGroup id="directory-splitter" direction="horizontal" class="bg-neutral-200">-->
-<!--        <SplitterPanel collapsible :collapsed-size="0" :min-size="10" :default-size="15" :max-size="20" class="p-2 pt-8">-->
-<!--            <div class="flex items-center justify-start gap-1 mb-2 w-full">-->
-<!--                <UDropdownMenu :items="[-->
-<!--                    {-->
-<!--                        label: 'In Document',-->
-<!--                        onSelect() {-->
-<!--                            handleCreatePage('root', currentPage)-->
-<!--                        }-->
-<!--                    },-->
-<!--                    {-->
-<!--                        label: 'At Same Level',-->
-<!--                        disabled: $slate.getPageParent(currentPage)?.uuid == null,-->
-<!--                        onSelect() {-->
-<!--                            handleCreatePage('current', currentPage)-->
-<!--                        }-->
-<!--                    },-->
-<!--                    {-->
-<!--                        label: 'Under Current Page',-->
-<!--                        onSelect() {-->
-<!--                            handleCreatePage('under', currentPage)-->
-<!--                        }-->
-<!--                    },-->
-<!--                ]">-->
-<!--                    <UButton icon="lucide:file-plus" variant="soft" size="sm" label="New Page..."/>-->
-<!--                </UDropdownMenu>-->
-<!--            </div>-->
-<!--            <USeparator class="mb-2"/>-->
-<!--            <TreeRoot-->
-<!--                :items="pages"-->
-<!--                v-slot="{ flattenItems }"-->
-<!--                class="list-none select-none w-full rounded-lg text-sm font-medium gap-1"-->
-<!--                :get-key="(item) => item.uuid"-->
-<!--                :expanded="$slate.getExpandedNodeCache().value"-->
-<!--            >-->
-<!--                <TreeItem-->
-<!--                    v-for="item in flattenItems"-->
-<!--                    v-slot="{ isExpanded, handleToggle }"-->
-<!--                    :key="item._id"-->
-<!--                    :style="{ 'padding-left': `${item.level - 1}rem` }"-->
-<!--                    v-bind="item.bind"-->
-<!--                    :class="['flex items-center rounded outline-none mb-1 gap-2']"-->
-<!--                    @toggle="(event) => {event.preventDefault()}"-->
-<!--                >-->
-<!--                    <UContextMenu size="sm" :items="[-->
-<!--                        [-->
-<!--                          {-->
-<!--                            label: 'Rename',-->
-<!--                            icon: 'lucide:pen-line',-->
-<!--                            onSelect: () => {-->
-<!--                                handleRename(item.value.uuid, item.value.name)-->
-<!--                            }-->
-<!--                          },-->
-<!--                          {-->
-<!--                            label: 'Change Icon',-->
-<!--                            icon: 'lucide:pen-line',-->
-<!--                            onSelect: () => {-->
-<!--                                handleChangeIcon(item.value.uuid)-->
-<!--                            }-->
-<!--                          },-->
-<!--                          {-->
-<!--                            label: 'Delete',-->
-<!--                            icon: 'lucide:trash-2',-->
-<!--                            color: 'error',-->
-<!--                            onSelect: () => {-->
-<!--                                handleDelete(item.value.uuid, true)-->
-<!--                            }-->
-<!--                          }-->
-<!--                        ],-->
-<!--                        [-->
-<!--                          {-->
-<!--                            label: 'New Child Page',-->
-<!--                            icon: 'lucide:file-plus',-->
-<!--                            onSelect: () => {-->
-<!--                                handleCreatePage('under', item.value.uuid)-->
-<!--                            }-->
-<!--                          }-->
-<!--                        ]-->
-<!--                      ]">-->
-<!--                        <UButton-->
-<!--                            class="justify-start w-full z-10"-->
-<!--                            :variant="currentPage == item.value.uuid ? 'solid' : 'ghost'"-->
-<!--                            :icon="item.value.icon"-->
-<!--                            :label="item.value.name"-->
-<!--                            size="sm"-->
-<!--                            @click="() => {-->
-<!--                                navigateTo(`/document/${item.value.uuid}`)-->
-<!--                            }"-->
-<!--                        />-->
-<!--                        <template v-if="(item.value.children?.length || 0) > 0">-->
-<!--                            <UButton-->
-<!--                                size="sm"-->
-<!--                                variant="subtle"-->
-<!--                                class="z-10" :icon="isExpanded ? 'lucide:folder-open': 'lucide:folder'"-->
-<!--                                @click="() => {-->
-<!--                                    if (isExpanded) {-->
-<!--                                        $slate.uncacheExpandedNode(item.value.uuid)-->
-<!--                                    } else {-->
-<!--                                        $slate.cacheExpandedNode(item.value.uuid)-->
-<!--                                    }-->
-<!--                                    handleToggle()-->
-<!--                                }"/>-->
-<!--                        </template>-->
-<!--                    </UContextMenu>-->
-<!--                </TreeItem>-->
-<!--            </TreeRoot>-->
-<!--        </SplitterPanel>-->
-<!--        <SplitterResizeHandle class="w-2"/>-->
-<!--        <SplitterPanel :min-size="20" class="rounded-l-2xl bg-neutral-50 m-2">-->
-<!--            <ScrollAreaRoot class="h-96">-->
-<!--                <ScrollAreaViewport>-->
-<!--                    <slot/>-->
-<!--                </ScrollAreaViewport>-->
-<!--                <ScrollAreaScrollbar-->
-<!--                    class="flex select-none touch-none p-0.5 z-20 bg-blackA1 transition-colors duration-[160ms] ease-out hover:bg-blackA2 data-[orientation=vertical]:w-2.5 data-[orientation=horizontal]:flex-col data-[orientation=horizontal]:h-2.5"-->
-<!--                    orientation="vertical"-->
-<!--                >-->
-<!--                    <ScrollAreaThumb-->
-<!--                        class="flex-1 bg-mauve10 rounded-[10px] relative before:content-[''] before:absolute before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2 before:w-full before:h-full before:min-w-[44px] before:min-h-[44px]"-->
-<!--                    />-->
-<!--                </ScrollAreaScrollbar>-->
-<!--                <ScrollAreaScrollbar-->
-<!--                    class="flex select-none touch-none p-0.5 bg-blackA6 transition-colors duration-[160ms] ease-out hover:bg-blackA8 data-[orientation=vertical]:w-2.5 data-[orientation=horizontal]:flex-col data-[orientation=horizontal]:h-2.5"-->
-<!--                    orientation="horizontal"-->
-<!--                >-->
-<!--                    <ScrollAreaThumb-->
-<!--                        class="flex-1 bg-mauve10 rounded-[10px] relative before:content-[''] before:absolute before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2 before:w-full before:h-full before:min-w-[44px] before:min-h-[44px]"-->
-<!--                    />-->
-<!--                </ScrollAreaScrollbar>-->
-<!--            </ScrollAreaRoot>-->
-<!--        </SplitterPanel>-->
-<!--    </SplitterGroup>-->
+    <div class="w-full fixed top-0 left-0 right-0 h-fit z-10">
+        <div class="flex items-center justify-center p-2 gap-2 select-none cursor-default" data-tauri-drag-region>
+            <div class="flex items-center justify-center p-1 px-2 gap-2 rounded-lg backdrop-blur-md z-20 w-fit" data-tauri-drag-region>
+                <!--Ignore The Error Here... fucking typescript-->
+                <UBreadcrumb :items="breadcrumbs" class="text-sm select-none gap-1" data-tauri-drag-region>
+                    <template #document="{item}" data-tauri-drag-region>
+                        <div data-tauri-drag-region class="px-1">{{item.label}}</div>
+                    </template>
+                    <template #page="{item}" data-tauri-drag-region>
+                        <UDropdownMenu :items="[
+                                        {
+                                            label: 'Rename Page',
+                                            onSelect () {
+                                                $slateCommon.renamePage($pageId, 'Page')
+                                            }
+                                        },
+                                        {
+                                            label: 'Change Icon',
+                                            onSelect () {
+                                                $slateCommon.changeIcon($pageId)
+                                            }
+                                        }
+                                    ]">
+                            <UButton data-tauri-drag-region class="px-1" :icon="item?.icon || 'lucide:file'" size="sm" :label="item?.label || 'Untitled Page'" variant="link" />
+                        </UDropdownMenu>
+                    </template>
+                    <template #dropdown="{item}" data-tauri-drag-region>
+                        <UDropdownMenu :items="(item as any).children || []">
+                            <UButton data-tauri-drag-region class="px-1" :icon="item.icon" size="sm" variant="link" />
+                        </UDropdownMenu>
+                    </template>
+                    <template #separator data-tauri-drag-region>
+                        <div class="text-(--ui-text-muted)" data-tauri-drag-region>/</div>
+                    </template>
+                </UBreadcrumb>
+                <Icon data-tauri-drag-region name="lucide:file-warning" class="size-3" v-if="$slate.getFilePath().value == null || ''"/>
+                <template v-else>
+                    <Icon data-tauri-drag-region name="lucide:file-clock" class="size-3 animate-pulse animate" v-if="$slate.isSavingFile().value"/>
+                    <Icon data-tauri-drag-region name="lucide:file-check" class="size-3" v-else-if="$slate.isFileSaved().value"/>
+                    <Icon data-tauri-drag-region name="lucide:file-x" class="size-3 bg-error-500" v-else/>
+                </template>
+            </div>
+        </div>
+    </div>
     <slot/>
 </template>
 
