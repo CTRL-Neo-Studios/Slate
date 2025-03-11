06eb49fa58fc1c74b33eaca3f9b2b6fbf5944f0d
Type-32 <87076491+Type-32@users.noreply.github.com>
2025-03-11 15:29:57 +0800

    chore: clearing stuff out

--- app/components/Slate/Editor/Text.vue
+++ app/components/Slate/Editor/Text.vue
@@ -221,50 +221,6 @@ function nodesView() {
 <template>
     <div class="w-full flex justify-center items-start min-h-screen">
         <div class="h-full sm:w-xl md:w-2xl w-3xl lg:w-4xl xl:w-5xl">
-            <div class="w-full fixed top-0 left-0 right-0 h-fit z-10">
-                <div class="flex items-center justify-center p-2 gap-2 select-none cursor-default" data-tauri-drag-region>
-                    <div class="flex items-center justify-center p-1 px-2 gap-2 rounded-lg backdrop-blur-md z-20 w-fit" data-tauri-drag-region>
-                        <!--Ignore The Error Here... fucking typescript-->
-                        <UBreadcrumb :items="breadcrumbs" class="text-sm select-none gap-1" data-tauri-drag-region>
-                            <template #document="{item}" data-tauri-drag-region>
-                                <div data-tauri-drag-region class="px-1">{{item.label}}</div>
-                            </template>
-                            <template #page="{item}" data-tauri-drag-region>
-                                <UDropdownMenu :items="[
-                                        {
-                                            label: 'Rename Page',
-                                            onSelect () {
-                                                $slateCommon.renamePage($pageId, 'Page')
-                                            }
-                                        },
-                                        {
-                                            label: 'Change Icon',
-                                            onSelect () {
-                                                $slateCommon.changeIcon($pageId)
-                                            }
-                                        }
-                                    ]">
-                                    <UButton data-tauri-drag-region class="px-1" :icon="item?.icon || 'lucide:file'" size="sm" :label="item?.label || 'Untitled Page'" variant="link" />
-                                </UDropdownMenu>
-                            </template>
-                            <template #dropdown="{item}" data-tauri-drag-region>
-                                <UDropdownMenu :items="(item as any).children || []">
-                                    <UButton data-tauri-drag-region class="px-1" :icon="item.icon" size="sm" variant="link" />
-                                </UDropdownMenu>
-                            </template>
-                            <template #separator data-tauri-drag-region>
-                                <div class="text-(--ui-text-muted)" data-tauri-drag-region>/</div>
-                            </template>
-                        </UBreadcrumb>
-                        <Icon data-tauri-drag-region name="lucide:file-warning" class="size-3" v-if="$slate.getFilePath().value == null || ''"/>
-                        <template v-else>
-                            <Icon data-tauri-drag-region name="lucide:file-clock" class="size-3 animate-pulse animate" v-if="$slate.isSavingFile().value"/>
-                            <Icon data-tauri-drag-region name="lucide:file-check" class="size-3" v-else-if="$slate.isFileSaved().value"/>
-                            <Icon data-tauri-drag-region name="lucide:file-x" class="size-3 bg-error-500" v-else/>
-                        </template>
-                    </div>
-                </div>
-            </div>
             <TiptapEditorContent :editor="$editor" class="max-w-none w-full h-full" />
             <template v-if="showBottomBar">
                 <div class="w-full fixed bottom-0 left-0 right-0 h-fit z-10">
